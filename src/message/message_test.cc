d009b4505ff1d68e3f608611329dc63610089ad4
Lukas Herman <lherman.cs@gmail.com>
2018-02-19 19:25:59 -0500

    Update test

--- src/message/message_test.cc
+++ src/message/message_test.cc
@@ -1,17 +1,20 @@
-#include <cstdio>
 #include "message.hpp"
+#include <cstdio>
 
 int main() {
   Message msg;
 
-  char data[] = "123456789";
+  char data[9];
+  int i = 0;
+  for (char c = '1'; c <= '9'; c++) data[i++] = c;
 
+  msg.write(data, sizeof(data));
   msg.write(data, sizeof(data));
 
   char buf[4];
 
   while (msg.read(buf, sizeof(buf)) > 0) {
-    printf("%s", buf);
+    printf("%s\n", buf);
   }
   printf("\n");
 
