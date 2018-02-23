b2e1753550e83adae13f0141e4d2d7832128ebbb
Lukas Herman <lherman.cs@gmail.com>
2018-02-23 11:29:32 -0500

    add log_msg

--- src/utils/utils.cc
+++ src/utils/utils.cc
@@ -7,3 +7,7 @@ void die(int err_code, std::string err_msg) {
   std::cerr << "Error(" << err_code << "): " << err_msg << std::endl;
   exit(err_code);
 }
+
+void log_msg(std::string name, std::string msg) {
+  std::cerr << name << ": " << msg << std::endl;
+}
--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -4,5 +4,6 @@
 #include <string>
 
 void die(int err_code, std::string err_msg);
+void log_msg(std::string name, std::string msg);
 
 #endif
